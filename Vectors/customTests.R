0e717e964ed673817101cc93d7dac73b2f428b7b
Jonathon Hill <jhill@byu.edu>
2022-08-18 09:59:05 -0600

    More bugs.

--- Vectors/customTests.R
+++ Vectors/customTests.R
@@ -1,34 +1,3 @@
-expr_creates_var <- function(correctName=NULL){
-  e <- get("e", parent.frame())
-  # TODO: Eventually make auto-detection of new variables an option.
-  # Currently it can be set in customTests.R
-  delta <- if(!customTests$AUTO_DETECT_NEWVAR){
-    safeEval(e$expr, e)
-  } else {
-    e$delta
-  }
-  if(is.null(correctName)){
-    results <- expectThat(length(delta) >= 1,
-                          testthat::is_true(),
-                          label=paste(deparse(e$expr), 
-                                      "does not create a variable."))  
-  } else {
-    results <- expectThat(correctName %in% names(delta), 
-                          testthat::is_true(), 
-                          label=paste(deparse(e$expr),
-                                      "does not create a variable named",
-                                      correctName))
-  }
-  if(results$passed){
-    e$newVar <- e$val
-    e$newVarName <- names(delta)[1]
-    e$delta <- mergeLists(delta, e$delta)
-  } else {
-    e$delta <- list()
-  }
-  return(results$passed)
-}
-
 readline_clean <- function(prompt = "") {
   wrapped <- strwrap(prompt, width = getOption("width") - 2)
   mes <- stringr::str_c("| ", wrapped, collapse = "\n")
