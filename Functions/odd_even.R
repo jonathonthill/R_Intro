943e65647acad1c48eac3477eba3608572cd1417
Jonathon Hill <jhill@byu.edu>
2022-08-23 08:38:11 -0600

    Added more lessons

---
+++ Functions/odd_even.R
@@ -0,0 +1,8 @@
+## Make a function to tell you Odd or Even
+# You will use this script for several questions.
+# Each time, you should save it, and then type submit() on the 
+# console.
+
+find_odd_even <- function(n) {
+  # put code here
+}
